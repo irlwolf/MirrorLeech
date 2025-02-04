f69da8ae4391f2316154dcb5ada95c91e27034bb
Spider-Man <theirlwolf@gmail.com>
2025-02-04 08:31:11 +0530

    Update update.py

--- update.py
+++ update.py
@@ -27,14 +27,14 @@ try:
 except:
     pass
 
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7931704916:AAF7z_qAWSg6E7MhXujjvwACDQZDuvZSEdg')
 if len(BOT_TOKEN) == 0:
     log_error("BOT_TOKEN variable is missing! Exiting now")
     exit(1)
 
 bot_id = BOT_TOKEN.split(':', 1)[0]
 
-DATABASE_URL = environ.get('DATABASE_URL', '')
+DATABASE_URL = environ.get('DATABASE_URL', 'jkd3o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 if len(DATABASE_URL) == 0:
     DATABASE_URL = None
 
